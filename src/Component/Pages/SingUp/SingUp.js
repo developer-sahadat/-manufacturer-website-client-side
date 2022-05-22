1ab4fee6fbdfa72c893fc9d5cda10e299fae5bd7
developer-sahadat <mdridoysikder862@gmail.com>
2022-05-22 20:42:46 +0600

    react environment variables set up

--- src/Component/Pages/SingUp/SingUp.js
+++ src/Component/Pages/SingUp/SingUp.js
@@ -1,48 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import google from "../../../Assets/Images/google.png";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import auth from "../../../FirebaseInit/FirebaseInit";
+
 const SingUp = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  console.log(user || error);
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    createUserWithEmailAndPassword(email, password);
+  };
+
   return (
     <div>
-      <section class="lg:h-screen lg:w-4/5 mx-auto py-10 lg:py-0">
-        <div class="container px-6 py-12 h-full">
-          <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-            <div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+      <section className="lg:h-screen lg:w-4/5 mx-auto py-10 lg:py-0">
+        <div className="container px-6 py-12 h-full">
+          <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+            <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
               <img
                 src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
-                class="w-full"
+                className="w-full"
                 alt="Phone_image"
               />
             </div>
-            <div class="md:w-8/12 lg:w-5/12 lg:ml-20">
-              <form>
-                <div class="mb-6">
+            <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
+              <form onSubmit={submitHandler}>
+                <div className="mb-6">
                   <input
+                    name="name"
                     required
                     type="text"
-                    class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     placeholder="Full Name"
                   />
                 </div>
-                <div class="mb-6">
+                <div className="mb-6">
                   <input
+                    name="email"
                     required
                     type="email"
-                    class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     placeholder="Email address"
                   />
                 </div>
 
-                <div class="mb-6">
+                <div className="mb-6">
                   <input
+                    name="password"
                     type="password"
-                    class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                     placeholder="Password"
                   />
                 </div>
 
-                <div class=" mb-6">
-                  <span class="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out">
+                <div className=" mb-6">
+                  <span className="text-blue-600 hover:text-blue-700 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out">
                     {" "}
                     Already Have an account?
                   </span>
@@ -51,19 +68,19 @@ const SingUp = () => {
 
                 <button
                   type="submit"
-                  class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
+                  className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
                   data-mdb-ripple="true"
                   data-mdb-ripple-color="light"
                 >
                   Sign in
                 </button>
               </form>
-              <div class="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
-                <p class="text-center font-semibold mx-4 mb-0">OR</p>
+              <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
+                <p className="text-center font-semibold mx-4 mb-0">OR</p>
               </div>
 
               <button
-                class="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
+                className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
                 style={{ backgroundColor: " #3b5998" }}
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
@@ -71,7 +88,7 @@ const SingUp = () => {
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 320 512"
-                  class="w-3.5 h-3.5 mr-2"
+                  className="w-3.5 h-3.5 mr-2"
                 >
                   <path
                     fill="currentColor"
@@ -81,7 +98,7 @@ const SingUp = () => {
                 Continue with Facebook
               </button>
               <a
-                class="px-7 py-3  font-medium text-sm leading-snug uppercase rounded border hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
+                className="px-7 py-3  font-medium text-sm leading-snug uppercase rounded border hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center"
                 style={{ backgroundColor: "white" }}
                 href="#!"
                 role="button"
