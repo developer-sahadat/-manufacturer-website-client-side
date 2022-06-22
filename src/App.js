4cf7a8494534e19209e6c9bd23ca10da0fef98aa
developer-sahadat <mdridoysikder862@gmail.com>
2022-06-22 22:43:51 +0600

    loading screen add

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import { ToastContainer } from "react-toastify";
 import AllRoutes from "./Component/Routes/AllRoutes";
 import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { useEffect, useState } from "react";
+import LoadingSpinner from "./Component/Shear/LoadingSpinner";
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoading = () => {
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("load", handleLoading);
+    return () => window.removeEventListener("load", handleLoading);
+  }, []);
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <AllRoutes />
+      {!isLoading ? (
+        <QueryClientProvider client={queryClient}>
+          <AllRoutes />
 
-        <ToastContainer />
-      </QueryClientProvider>
+          <ToastContainer />
+        </QueryClientProvider>
+      ) : (
+        <LoadingSpinner />
+      )}
     </>
   );
 }
